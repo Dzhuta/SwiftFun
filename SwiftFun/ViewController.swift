9e39d89990a0ebca1dce9718a40647af1f4d64ca
Juta <juta.zalite@gmail.com>
2021-01-30 14:09:01 +0200

    Our first commit as gitters!

---
+++ SwiftFun/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class ViewController: UIViewController {
+    var buttonCount = 0
+    @IBOutlet weak var myLabel: UILabel!
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        // buttonCount += 1
+
+        print(buttonCount)
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            myLabel.text = "You hit it more than 10 times!"
+        }
+        if buttonCount >= 15 {
+            view.backgroundColor = UIColor.green
+            myLabel.text = "You hit it more than 15 times!"
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
